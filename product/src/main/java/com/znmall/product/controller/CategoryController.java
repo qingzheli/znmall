13cc67292ad64a4901e666830829a2c660ccd1ef
qingzheli <liqingzhe86@gmail.com>
2020-08-12 20:38:51 -0400

    build 3-level categories structure

--- product/src/main/java/com/znmall/product/controller/CategoryController.java
+++ product/src/main/java/com/znmall/product/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.znmall.product.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ public class CategoryController {
         PageUtils page = categoryService.queryPage(params);
 
         return R.ok().put("page", page);
+    }
+
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+
+        return R.ok().put("data", entities);
     }
 
 
--- product/src/main/java/com/znmall/product/service/impl/CategoryServiceImpl.java
+++ product/src/main/java/com/znmall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.znmall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +28,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+//        1. query all categories
+        List<CategoryEntity> entities  = baseMapper.selectList(null);
+
+//        2. build tree structure for categories
+//        2.1. find all level-1 category
+        List<CategoryEntity> level1Menus = entities.stream().filter((categoryEntity -> categoryEntity.getParentCid() == 0))
+                .map(category->{
+                    category.setChildren(getChildren(category,entities));
+                    return category;
+                })
+                .sorted((category1, category2)->(category1.getSort()==null?0:category1.getSort())-(category2.getSort()==null?0:category2.getSort()))
+                .collect(Collectors.toList());
+
+
+        return level1Menus;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity parentCategory, List<CategoryEntity> entities) {
+        return entities.stream().filter((categoryEntity -> categoryEntity.getParentCid()==parentCategory.getCatId()))
+                .map(childCategory->{
+                    childCategory.setChildren(getChildren(childCategory, entities));
+                    return childCategory;
+                })
+                .sorted((category1, category2)->(category1.getSort()==null?0:category1.getSort())-(category2.getSort()==null?0:category2.getSort()))
+                .collect(Collectors.toList());
     }
 
 }
